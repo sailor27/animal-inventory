8c73d521f288868f16975caafc71f7430bb3e7ef
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-19 15:21:20 -0800

    Add select menu with drop down options for young and mature animals to animal-list component. Set filterByAge property on animal-list component to default. Create onChange method accepting selected option as argument. Mark allAnimals selected in drop down menu.

--- src/app/animal-list.component.ts
+++ src/app/animal-list.component.ts
@@ -4,9 +4,16 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'animal-list',
   template: `
+
+  <div class="animal-options">
+    <select (change)="onChange($event.target.value)">
+      <option value="allAnimals" selected="selected"> All Animals </option>
+      <option value="youngAnimals"> Young Animals </option>
+      <option value="matureAnimals"> Mature Animals </option>
+    </select>
+  </div>
   <div class="animal-card">
-    <h2>Animals</h2>
-    <h2>Test Animal: {{childAnimalList[2].name}} is 1 year old</h2>
+    <h2>Test Animal: {{childAnimalList[1].name}} is 1 year old</h2>
     <ul *ngFor= "let currentAnimal of childAnimalList | age">
       <h3>{{currentAnimal.species}}</h3>
       <li>Name: {{currentAnimal.name}}</li>
@@ -17,15 +24,23 @@ import { Animal } from './animal.model';
       <li>Sex: {{currentAnimal.sex}}</li>
       <li>Likes: {{currentAnimal.likes}}</li>
       <li>Dislikes: {{currentAnimal.dislikes}}</li>
+
       <button (click)="editAnimalButtonHasBeenClicked(currentAnimal)"> Edit </button>
     </ul>
   </div>
+
   `
 })
 
 export class AnimalListComponent{
   @Input() childAnimalList: Animal[];
   @Output() clickSender = new EventEmitter();
+
+  filterByAge: string = "allAnimals";
+
+  onChange(optionFromMenu) {
+    this.filterByAge = optionFromMenu;
+  }
 
   editAnimalButtonHasBeenClicked(animalToEdit: Animal){
     this.clickSender.emit(animalToEdit);
