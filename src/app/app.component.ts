36d8cfb64c35fd17df9c2544fb4189d93585dfe9
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-19 10:01:07 -0800

    Set up development environment using npm, bower, angular/cli and serve. Add plan to README.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <div class="container">
+    <div class="jumbotron">
+      <h1>Animal Inventory</h1>
+      <h5>View current animals, edit animal info, and add new animals</h5>
+    </div>
+  </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
