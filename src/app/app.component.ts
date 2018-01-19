7ed7cd9695aeb14bcf634b72f00247099e6e6730
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-19 10:38:04 -0800

    Add edit animal function to root component class with an alert.  Attach edit animal button on animal entries and bind edit function to click event, and test.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,22 @@ import { Component } from '@angular/core';
       <h5>View current animals, edit animal info, and add new animals</h5>
     </div>
     <div class="animals">
-      <h2>Animals</h2>
-      <ul>
-        <h3>{{currentAnimal}}</h3>
-        <li *ngFor= "let currentAnimal of animals">{{currentAnimal.name}}</li>
-      </ul>
-    </div>
+      <div class="animal-card">
+        <h2>Animals</h2>
+        <ul *ngFor= "let currentAnimal of animals">
+          <h3>{{currentAnimal.species}}</h3>
+          <li>Name: {{currentAnimal.name}}</li>
+          <li>Age: {{currentAnimal.age}}</li>
+          <li>Diet: {{currentAnimal.diet}}</li>
+          <li>Location: {{currentAnimal.location}}</li>
+          <li>Caretakers: {{currentAnimal.caretakers}}</li>
+          <li>Sex: {{currentAnimal.sex}}</li>
+          <li>Likes: {{currentAnimal.likes}}</li>
+          <li>Dislikes: {{currentAnimal.dislikes}}</li>
+          <button (click)="editAnimal()"> Edit </button>
+        </ul>
+      </div> <!-->animal-card<-->
+    </div><!-->animals<-->
   </div>`,
   styleUrls: ['./app.component.css']
 })
@@ -34,6 +44,9 @@ export class AppComponent {
     new Animal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Laying in the sunshine", "Toys that are not rope-based"),
     new Animal("Northwest Black-Tailed Deer", "Tinkerbell", 8, "Herbivore", "Northern Trail", 2, "Female", "Delicate roots and leaves", "Loud Nouises")
   ];
+  editAnimal(){
+    alert("you want to edit, huh? good news: this feature is coming soon");
+  }
 
 }
 
