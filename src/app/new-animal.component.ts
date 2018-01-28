9d86ed1aab18e73f09d78a2c7b025ef2e9524c7b
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-27 17:12:33 -0800

    Style animal list and cards using scss.

--- src/app/new-animal.component.ts
+++ src/app/new-animal.component.ts
@@ -5,7 +5,7 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'new-animal',
   templateUrl: './new-animal.component.html',
-	styleUrls: ['./new-animal.component.scss']
+	styleUrls: ['./edit-animal.component.scss']
 })
 
 export class NewAnimalComponent {
